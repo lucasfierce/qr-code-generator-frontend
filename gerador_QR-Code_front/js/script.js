6203c40b2a3eedea9c3a85ab1ada27dfbc1aa955
lucasfierce <147957174+lucasfierce@users.noreply.github.com>
2026-04-26 22:39:26 -0300

    HTML, CSS, and JS added

---
+++ gerador_QR-Code_front/js/script.js
@@ -0,0 +1,35 @@
+const mainContainer = document.querySelector(".main-container");
+const qrCodeBtn = document.querySelector("#qr-button");
+const qrCodeInput = document.querySelector("#qr-input");
+const qrCodeImg = document.querySelector("#qr-code img");
+
+function generateQRCode(){
+    if(!qrCodeInput) return;
+
+    qrCodeBtn.innerText = 'Gerando QR Code...';
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInput.value}`
+
+    if(qrCodeInput){
+        mainContainer.classList.add("active");
+        qrCodeBtn.innerText = "Código criado!"
+    }
+
+    console.log(qrCodeInput.value);
+}
+qrCodeBtn.addEventListener("click",() => {
+    generateQRCode();
+})
+
+qrCodeInput.addEventListener("keydown", (e) => {
+    if(e.key === 'Enter'){
+        generateQRCode();
+    }
+})
+
+qrCodeInput.addEventListener("keyup", () =>{
+    if(!qrCodeInput.value){
+        mainContainer.classList.remove("active");
+        qrCodeBtn.innerText = "Gerar QR Code"
+    }
+})
